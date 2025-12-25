52208fa64253d99b529d23a4f4636ac678eda028
dimm999 <dimawebmaker@gmail.com>
2025-12-25 15:16:05 +0500

    post and category components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,35 +1,40 @@
 <x-app-layout>
-
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-4">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-4 text-gray-900">
+                    <ul
+                        class="justify-center flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active"
+                                aria-current="page">All</a>
+                        </li>
+                        @foreach ($categories as $category)
+                            <li class="me-2">
+                                <a href="#"
+                                    class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">{{ $category->name }}</a>
+                            </li>
+                        @endforeach
 
-
-                    
-
-
-<ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-    <li class="me-2">
-        <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active" aria-current="page">Tab 1</a>
-    </li>
-    <li class="me-2">
-        <a href="#"  class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab 2</a>
-    </li>
-    <li class="me-2">
-        <a href="#" class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab 3</a>
-    </li>
-    <li class="me-2">
-        <a href="#" class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab 4</a>
-    </li>
-    <li>
-        <a class="inline-block px-4 py-3 text-gray-400 cursor-not-allowed dark:text-gray-500">Tab 5</a>
-    </li>
-</ul>
-
+                    </ul>
                 </div>
             </div>
+
+            <div class="mt-8">
+                <div class="p-4">
+                    <ul class="flex flex-wrap text-sm font-medium text-gray-500 dark:text-gray-400">
+                        @forelse ($posts as $p)
+                            <x-post-item :post="$p" />
+                        @empty
+                            <div>
+                                <p class="text-gray-900">No posts found.</p>
+                            </div>
+                        @endforelse
+                    </ul>
+                </div>
+                {{ $posts->links() }}
+            </div>
+
         </div>
     </div>
 </x-app-layout>
