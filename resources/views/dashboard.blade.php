db38f8469e1e22501d0497a6dbfc43ac4d32a86e
dimm999 <dimawebmaker@gmail.com>
2025-12-27 22:06:46 +0500

    Category component and logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,20 +3,9 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-4 text-gray-900">
-                    <ul
-                        class="justify-center flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-                        <li class="me-2">
-                            <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active"
-                                aria-current="page">All</a>
-                        </li>
-                        @foreach ($categories as $category)
-                            <li class="me-2">
-                                <a href="#"
-                                    class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">{{ $category->name }}</a>
-                            </li>
-                        @endforeach
-
-                    </ul>
+                    <x-category-tabs>
+                        No Categories
+                    </x-category-tabs>
                 </div>
             </div>
 
@@ -27,7 +16,7 @@
                             <x-post-item :post="$p" />
                         @empty
                             <div>
-                                <p class="text-gray-900">No posts found.</p>
+                                <p class="text-center text-gray-400 py-16">No posts found.</p>
                             </div>
                         @endforelse
                     </ul>
