d2d56a3c26370c01e89ee012d78740e9bed2fb87
Dimm <dimawebmaker@gmail.com>
2026-01-06 01:20:26 +0500

    follow function and claps model

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -9,11 +9,15 @@
                 <div class="flex gap-4">
                     <x-user-avatar :user="$post->user" />
                     <div>
-                        <div class="flex gap-2">
+                        <x-follow-ctr :user="$post->user" class="flex gap-2">
                             <a href="{{ route('profile.show', $post->user) }}" class="hover:underline">{{ $post->user->username }}</a>
                             &middot;
-                            <a href="#" class="text-emerald-500">Follow</a>
-                        </div>
+                            <button
+                                x-text="following ? 'Unfollow' : 'Follow'"
+                                :class="following ? 'text-red-600' : 'text-emerald-600'"
+                                @click="follow()"
+                            />
+                        </x-follow-ctr>
                         <div class="flex gap-2 text-sm text-gray-500">
                             {{ $post->readTime() }} min read
                             &middot;
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -18,19 +18,26 @@
                     </div>
                     {{-- End Main Section --}}
                     {{-- Sidebar --}}
-                    <div class="w-[320px] border-l px-8">
+                    <x-follow-ctr :user="$user">
                         <x-user-avatar :user="$user" size="w-24 h-24" />
                         <h3>{{ $user->name }}</h3>
-                        <p class="text-gray-500">26k followers</p>
+                        <p class="text-gray-500">
+                            <span x-text="followersCount"></span>
+                            followers
+                        </p>
                         <p class="">
                             {{ $user->bio }}
                         </p>
-                        <div class="mt-4">
-                            <button class="bg-emerald-400 rounded-full px-4 py-2 text-white">
-                                Follow
-                            </button>
-                        </div>
-                    </div>
+                        @if(auth()->user() && auth()->user()->id !== $user->id)
+                            <div class="mt-4">
+                                <button class="rounded-full px-4 py-2 text-white"
+                                    @click="follow()"
+                                    x-text="following ? 'Unfollow' : 'Follow'" 
+                                    :class="following ? 'bg-red-500' : 'bg-emerald-400'" 
+                                />
+                            </div>
+                        @endif
+                    </x-follow-ctr>
                     {{-- End Sidebar --}}
                 </div>
             </div>
