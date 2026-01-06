9ca2884fa2ababe41a063ca3d6e2adf8f9eeaedf
Dimm <dimawebmaker@gmail.com>
2026-01-06 14:29:25 +0500

    add spatie media library

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'image',
@@ -19,6 +23,13 @@ class Post extends Model
         'user_id',
         'published_at',
     ];
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->width(400);
+    }
 
     public function user()
     {
